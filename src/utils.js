7aa3c6448cc390a91dfad4abba2a83de757c238b
beMimg <bernardommguerreiro@gmail.com>
2023-12-02 22:44:27 +0100

    fixed form modal display

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 function handleProjectModal() {
   const projectFormModal = document.querySelector('.project-form-modal');
   if (!projectFormModal.classList.contains('open')) {
+    projectFormModal.classList.remove('close');
     projectFormModal.classList.add('open');
   } else if (projectFormModal.classList.contains('open')) {
     projectFormModal.classList.remove('open');
